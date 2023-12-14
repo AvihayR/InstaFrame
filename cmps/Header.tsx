6fd4d19f51fb74e0b8e355bc0a4c4ab5b3b8f8b6
AvihayR <avihayrabi@gmail.com>
2023-12-14 11:02:12 +0200

    Setup base for the project

---
+++ cmps/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header>
+            <h1 className="flex justify-center px-4 text-slate-400 sm:text-orange-500 md:text-lime-500 lg:text-cyan-300">
+                Header
+            </h1>
+        </header>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './cmps/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  mode: 'jit',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+}
+

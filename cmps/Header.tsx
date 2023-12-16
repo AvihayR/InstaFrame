4a6aa7b1be14a4d1f0bc17126191ab5cce80b8ca
AvihayR <avihayrabi@gmail.com>
2023-12-16 16:09:08 +0200

    Add NavBar Media Queries

--- cmps/Header.tsx
+++ cmps/Header.tsx
@@ -1,17 +1,18 @@
 import localFont from 'next/font/local'
-import Image from 'next/image'
 export const instaFont = localFont({ src: '../assets/fonts/insta.ttf' })
+
 import HeartIcon from './icons/HeartIcon'
 import { NavBar } from './NavBar'
 
 
 export function Header() {
+
     return (
         <header className='app-header flex px-2 py-4 border-b justify-center xs:px-4 md:border-b-0 md:border-r'>
             <NavBar />
-            <div className="search-container flex items-center gap-5 md:hidden">
+            <div className="search-container flex flex-1 items-center gap-5 md:hidden">
                 <HeartIcon className='' />
-                <input type="text" className='search flex grow rounded-md px-2' placeholder='Search..' />
+                <input type="text" className='search max-w-xs flex grow rounded-md px-2' placeholder='Search..' />
             </div>
             <div className="logo-container hidden grow justify-end xs:flex md:hidden">
                 <h1 className={`logo text-2xl ${instaFont.className} `}>
--- cmps/NavBar.tsx
+++ cmps/NavBar.tsx
@@ -1,3 +1,4 @@
+import { instaFont } from "./Header"
 import CreateIcon from "./icons/CreateIcon"
 import ExploreIcon from "./icons/ExploreIcon"
 import HeartIcon from "./icons/HeartIcon"
@@ -8,14 +9,32 @@ import SearchIcon from "./icons/SearchIcon"
 
 export function NavBar() {
     return (
-        <div className="nav-container hidden md:flex flex-col gap-8">
-            <InstaIcon />
-            <nav className="flex flex-col gap-2">
-                <HomeIcon />
-                <SearchIcon />
-                <ExploreIcon />
-                <HeartIcon />
-                <CreateIcon />
+        <div className="nav-container fixed inset-x-0 bottom-0 flex flex-1 md:flex md:justify-start md:static md:flex-col gap-8 lg:w-56">
+            <div className="icon-container items-end hidden md:flex">
+                <InstaIcon className="lg:hidden" />
+                <h1 className={`hidden text-xl ${instaFont.className} lg:block`}>InstaFrame</h1>
+            </div>
+            <nav className="flex flex-1 py-1 justify-evenly border-t md:justify-start md:border-t-0 md:gap-2 md:flex-col">
+                <div className="icon-container flex items-end">
+                    <HomeIcon />
+                    <span className="hidden ms-4 lg:block">Home</span>
+                </div>
+                <div className="icon-container flex items-end">
+                    <SearchIcon />
+                    <span className="hidden ms-4 lg:block">Search</span>
+                </div>
+                <div className="icon-container flex items-end">
+                    <ExploreIcon />
+                    <span className="hidden ms-4 lg:block">Explore</span>
+                </div>
+                <div className="icon-container flex items-end">
+                    <HeartIcon />
+                    <span className="hidden ms-4 lg:block">Notifications</span>
+                </div>
+                <div className="icon-container flex items-end">
+                    <CreateIcon />
+                    <span className="hidden ms-4 lg:block">Create</span>
+                </div>
             </nav>
         </div>
     )

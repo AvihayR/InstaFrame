949b02d8d6475f1f5d91c2da2c62020ab6a3c720
AvihayR <avihayrabi@gmail.com>
2023-12-16 22:52:18 +0200

    Add NavLinks to improve DRY Principles

--- cmps/NavBar.tsx
+++ cmps/NavBar.tsx
@@ -1,10 +1,6 @@
 import { instaFont } from "./Header"
-import CreateIcon from "./icons/CreateIcon"
-import ExploreIcon from "./icons/ExploreIcon"
-import HeartIcon from "./icons/HeartIcon"
-import HomeIcon from "./icons/HomeIcon"
 import InstaIcon from "./icons/InstaIcon"
-import SearchIcon from "./icons/SearchIcon"
+import NavLinks from "./NavLinks"
 
 
 export function NavBar() {
@@ -15,26 +11,7 @@ export function NavBar() {
                 <h1 className={`hidden text-xl ${instaFont.className} lg:block`}>InstaFrame</h1>
             </div>
             <nav className="flex flex-1 py-1 justify-evenly border-t md:justify-start md:border-t-0 md:gap-2 md:flex-col">
-                <div className="icon-container flex items-end">
-                    <HomeIcon />
-                    <span className="hidden ms-4 lg:block">Home</span>
-                </div>
-                <div className="icon-container flex items-end">
-                    <SearchIcon />
-                    <span className="hidden ms-4 lg:block">Search</span>
-                </div>
-                <div className="icon-container flex items-end">
-                    <ExploreIcon />
-                    <span className="hidden ms-4 lg:block">Explore</span>
-                </div>
-                <div className="icon-container flex items-end">
-                    <HeartIcon />
-                    <span className="hidden ms-4 lg:block">Notifications</span>
-                </div>
-                <div className="icon-container flex items-end">
-                    <CreateIcon />
-                    <span className="hidden ms-4 lg:block">Create</span>
-                </div>
+                <NavLinks />
             </nav>
         </div>
     )
---
+++ cmps/NavLinks.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import CreateIcon from "./icons/CreateIcon"
+import ExploreIcon from "./icons/ExploreIcon"
+import HeartIcon from "./icons/HeartIcon"
+import HomeIcon from "./icons/HomeIcon"
+import SearchIcon from "./icons/SearchIcon"
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
+
+const links = [
+    { name: 'Home', href: '/', icon: HomeIcon },
+    { name: 'Search', href: '', icon: SearchIcon, },
+    { name: 'Explore', href: '/explore', icon: ExploreIcon },
+    { name: 'Notifications', href: '', icon: HeartIcon },
+    { name: 'Create', href: '/create', icon: CreateIcon },
+]
+
+export default function NavLinks() {
+    const path = usePathname()
+
+    return (
+        <>
+            {links.map((link) => {
+                const LinkIcon = link.icon
+                return (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className={
+                            clsx(
+                                "icon-container flex items-end",
+                                {
+                                    'active': path === link.href
+                                },
+                            )
+                        }
+                    >
+                        <LinkIcon className="w-6" />
+                        <span className="hidden ms-4 lg:block">{link.name}</span>
+                    </Link>
+                )
+            })}
+        </>
+    )
+}

6b9794650cf4eb9bd675dbd5846555931e6ace8f
AvihayR <avihayrabi@gmail.com>
2023-12-17 20:18:01 +0200

    Add Posts initial interface types

---
+++ cmps/ExploreList.tsx
@@ -0,0 +1,9 @@
+import { Post, Comment } from '../typings';
+
+
+export function ExploreList(posts: Post[]) {
+
+    return (
+        <h1>Posts</h1>
+    )
+}
---
+++ typings.ts
@@ -0,0 +1,15 @@
+export interface Post {
+    _id: string
+    userId: string
+    caption: string
+    imgUrls: string[]
+    vidUrls: string[]
+    likedBy: string[]
+    comments: Comment[]
+}
+
+export interface Comment {
+    by: string
+    txt: string
+    likes: string[]
+}

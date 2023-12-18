d39688f1578bb186c2347829f0e827f8a97a897b
AvihayR <avihayrabi@gmail.com>
2023-12-18 23:22:55 +0200

    Add MultiImgIcon to indicate posts with multiple imgs

--- cmps/ExplorePreview.tsx
+++ cmps/ExplorePreview.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Post, Comment } from '../typings'
+import MultiImgIcon from './icons/MultiImgIcon'
 interface ExplorePreviewProps {
     post: Post
 }
@@ -8,7 +9,12 @@ const ExplorePreview: React.FC<ExplorePreviewProps> = ({ post }) => {
 
     return (
         <div className="preview-card relative">
-            <div className='shadow flex bg-black absolute w-full h-full cursor-pointer transition delay-50 opacity-0 hover:opacity-30' />
+            {post.imgUrls.length > 1 && (
+                <div className="icon-container absolute  top-2 right-2">
+                    <MultiImgIcon className='drop-shadow-2xl' />
+                </div>
+            )}
+            <div className='shadow bg-black absolute w-full h-full cursor-pointer transition delay-50 opacity-0 hover:opacity-30' />
             <Image className="explore-img aspect-square object-cover" src={post.imgUrls[0]} alt='image' width={316} height={316} />
         </div>
     )

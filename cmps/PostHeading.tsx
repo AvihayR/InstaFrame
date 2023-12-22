2144a949bd1e06e6ec5963795dadd5f96756badf
AvihayR <avihayrabi@gmail.com>
2023-12-22 23:02:58 +0200

    Optimize postDetails and Preview to Dark&Light color prefrences

--- cmps/PostHeading.tsx
+++ cmps/PostHeading.tsx
@@ -14,8 +14,8 @@ export function PostHeading({ post }: PostHeadingProps) {
             <div className="profile-container flex items-center">
                 {post?.by && <Image className='profile-img cursor-pointer' src={post.by.userImg} width={32} height={32} alt='Profile image' />}
                 {post?.by && <h3 className='ms-3.5 font-medium cursor-pointer hover:text-gray-400'>{post.by.username}</h3>}
-                <span className='block mx-2 text-xs'>•</span>
-                <Link href={''} className='text-link-blue hover:text-slate-50'>Follow</Link>
+                <span className='divider block mx-1 text-sm text-gray-500'>•</span>
+                <Link href={''} className='text-link-blue font-medium hover:text-sky-600  dark:hover:text-slate-50 '>Follow</Link>
             </div>
             <div className="icon-container hover:text-gray-400 cursor-pointer">
                 <ThreeDots />

1c9095246243eaaa0c952d77474a0bdf1a5691cc
AvihayR <avihayrabi@gmail.com>
2023-12-26 16:32:56 +0200

    Add Login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,22 @@
 'use client'
-
 import { userService } from "@/services/user.service.local"
 import { UserCreds, UserToken } from '../../typings'
 import { useEffect, useState } from "react"
+import { instaFont } from "@/cmps/Header"
+import Link from "next/link"
 
 export default function LoginPage() {
     const [loggedUser, setLoggedUser] = useState<UserToken | null>(null)
+    const [isSignUpMode, setIsSignUpMode] = useState(false)
 
     useEffect(() => {
         setLoggedUser(userService.getLoggedinUser())
     }, [])
 
+
+    function toggleSignUpLogin() {
+        setIsSignUpMode(currState => !currState)
+    }
 
     async function onLogin(formData: FormData) {
         let userCreds: UserCreds = { username: '', password: '' }
@@ -36,11 +42,34 @@ export default function LoginPage() {
                     <button onClick={onLogout}>Log out</button>
                 </>
                 :
-                <form className="login-form" action={onLogin}>
-                    <input type="text" name="username" placeholder="Username or email" />
-                    <input type="text" name="password" placeholder="Password" />
-                    <button type="submit">Login</button>
-                </form>
+                <>
+                    <form className="login-form grid place-items-center border border-zinc-800 p-10 mt-28" action={onLogin}>
+                        <div className="inputs-container grid">
+                            <h1 className={`text-3xl mx-auto mb-9 ${instaFont.className}`}>InstaFrame</h1>
+                            <input className="mb-2 w-60 bg-zinc-800 text-xs p-2 border border-zinc-900 rounded" type="text" name="username" placeholder="Username or email" />
+                            {isSignUpMode && <input className="mb-2 w-60 bg-zinc-800 text-xs p-2 border border-zinc-900 rounded" type="text" name="username" placeholder="Full name" />}
+                            <input className="mb-5 w-60 bg-zinc-800 text-xs p-2 border border-zinc-900 rounded" type="text" name="password" placeholder="Password" />
+                            {isSignUpMode ?
+                                <button className="font-medium text-sm bg-sky-400 rounded-md p-1 mx-1" type="submit">Sign up</button>
+                                :
+                                <button className="font-medium text-sm bg-sky-400 rounded-md p-1 mx-1" type="submit">Login</button>}
+
+                        </div>
+                    </form>
+                    <div className="switch-method flex justify-center border border-zinc-800 p-5 mt-2 text-sm">
+                        {isSignUpMode ?
+                            <>
+                                <h1>Already have an account?</h1>
+                                <Link href="" className="text-sky-400 ml-1" onClick={toggleSignUpLogin}>Sign in</Link>
+                            </>
+                            :
+                            <>
+                                <h1>Don't have an account yet?</h1>
+                                <Link href="" className="text-sky-400 ml-1" onClick={toggleSignUpLogin}>Signup</Link>
+                            </>
+                        }
+                    </div>
+                </>
             }
 
         </section>

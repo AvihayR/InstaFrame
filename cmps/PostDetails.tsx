a393114947e7961ee6688db92e4fdb0c10974e1a
AvihayR <avihayrabi@gmail.com>
2023-12-30 20:48:45 +0200

    Add scrollable comment-list in postDetails

--- cmps/PostDetails.tsx
+++ cmps/PostDetails.tsx
@@ -49,20 +49,24 @@ export function PostDetails({ post }: PostDetailsProps) {
 
             <section className='comments-section'>
                 <PostHeading post={post} />
-                <article className='post-caption flex items-start px-4 py-3'>
-                    {post?.by && <Image className='profile-img cursor-pointer rounded-3xl' src={post.by.userImg} width={30} height={30} alt='Profile image' />}
-                    <div className="caption-container grid ms-2">
-                        <div className="user-container flex items-end">
-                            {post?.by && <h3 className='font-medium text-sm cursor-pointer leading-none hover:text-gray-400'>{post.by.username}</h3>}
+
+                <div className="comment-list">
+                    <article className='post-caption flex items-start px-4 py-3'>
+                        {post?.by && <Image className='profile-img cursor-pointer rounded-3xl' src={post.by.userImg} width={30} height={30} alt='Profile image' />}
+                        <div className="caption-container grid ms-2">
+                            <div className="user-container flex items-end">
+                                {post?.by && <h3 className='font-medium text-sm cursor-pointer leading-none hover:text-gray-400'>{post.by.username}</h3>}
+                            </div>
+                            {post?.caption &&
+                                <span className='caption text-sm'>
+                                    <TxtBreaks str={post.caption} />
+                                </span>}
+                            {post?.postedAt && <span className='time-ago mt-1 text-xs font-thin leading-none text-gray-400'>{timeAgo(post.postedAt)}</span>}
                         </div>
-                        {post?.caption &&
-                            <span className='caption text-sm'>
-                                <TxtBreaks str={post.caption} />
-                            </span>}
-                        {post?.postedAt && <span className='time-ago mt-1 text-xs font-thin leading-none text-gray-400'>{timeAgo(post.postedAt)}</span>}
-                    </div>
-                </article>
-                <CommentList comments={post?.comments} onLikeComment={onLikeComment} onUnLikeComment={onUnLikeComment} />
+                    </article>
+                    <CommentList comments={post?.comments} onLikeComment={onLikeComment} onUnLikeComment={onUnLikeComment} />
+                </div>
+
                 <PostActionsBar />
             </section>
         </div>

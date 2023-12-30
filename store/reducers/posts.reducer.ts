37d321e0214c3482efb2f3b6ed02315d60a81763
AvihayR <avihayrabi@gmail.com>
2023-12-30 13:55:20 +0200

    Add posts reducer

---
+++ store/reducers/posts.reducer.ts
@@ -0,0 +1,57 @@
+import { Post } from "@/typings"
+
+export const SET_POSTS = 'SET_POSTS'
+export const ADD_POST = 'ADD_POST'
+export const REMOVE_POST = 'REMOVE_POST'
+export const UPDATE_POST = 'UPDATE_POST'
+// export const SET_FILTER_BY = 'SET_FILTER_BY'
+
+interface Action {
+    type?: string
+    post?: Post
+    posts?: Post[]
+    postId?: string
+}
+
+interface State {
+    posts: Post[]
+    lastRemovedPost: Post | null
+}
+
+const initialState: State = {
+    posts: [],
+    lastRemovedPost: null
+}
+
+export function postReducer(state = initialState, action: Action = {}) {
+    let tempPosts: Post[] = []
+    let newState = state
+
+    switch (action.type) {
+        case SET_POSTS:
+            newState = { ...state, posts: action.posts || [] }
+            break
+
+        case ADD_POST:
+            if (action.post)
+                newState = { ...state, posts: [...state.posts, action.post] }
+            break
+
+        case REMOVE_POST:
+            const removedPost = state.posts.find(post => post._id === action.postId)
+            const filteredPosts = state.posts.filter(post => post._id !== action.postId)
+
+            if (removedPost)
+                newState = { ...state, posts: filteredPosts, lastRemovedPost: removedPost }
+            break
+
+        case UPDATE_POST:
+            if (action.post) {
+                tempPosts = state.posts.map(post => (post._id === action.post?._id) ? action.post : post)
+                newState = { ...state, posts: tempPosts }
+            }
+            break
+        default:
+    }
+    return newState
+}

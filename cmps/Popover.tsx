b212726530143c2b9da8040ffd67d94c46e39f20
AvihayR <avihayrabi@gmail.com>
2024-01-05 23:53:39 +0200

    Stop propogating click event to keep popover open

--- cmps/Popover.tsx
+++ cmps/Popover.tsx
@@ -7,9 +7,13 @@ interface PopoverProps {
 }
 
 export function Popover({ isOpen, children }: PopoverProps) {
+    function handleContentClick(event: React.MouseEvent<HTMLElement>) {
+        event.stopPropagation()
+    }
+
     return (
         isOpen &&
-        <div className='popover-content absolute bottom-6 px-4 py-3 shadow-md rounded-xl bg-light dark:bg-dark'>
+        <div onClick={handleContentClick} className='popover-content absolute bottom-6 px-4 py-3 shadow-md rounded-xl bg-light dark:bg-dark'>
             {children}
         </div>
     )
